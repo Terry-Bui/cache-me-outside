bbe35188224d3b63674b828fc7904277889e8b95
Terry Bui <=>
2018-06-13 02:03:07 +1000

    format code to standard

--- services/redis.js
+++ services/redis.js
@@ -1,7 +1,7 @@
 const redis = require('redis')
 const client = redis.createClient({
-      host: process.env.REDIS_HOST
-    })
+  host: process.env.REDIS_HOST
+})
 
 const {promisify} = require('util')
 const getAsync = promisify(client.get).bind(client)
