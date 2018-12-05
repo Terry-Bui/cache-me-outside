aa541898ea58d1ab66669912250983e212490bd0
Terry Bui <terry.bui96@gmail.com>
2018-12-05 12:19:58 +1100

    upgrade to babel 7 add try catch block remove render

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ var logger = require('morgan')
 
 var app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set('json spaces', 2) // number of spaces for indentation
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -32,8 +30,10 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: err
+  })
 })
 
 module.exports = app
--- places/placesController.js
+++ places/placesController.js
@@ -2,21 +2,27 @@ const redis = require('./../services/redis')
 const places = require('./../services/google-places')
 
 module.exports.getPlace = async (req, res) => {
-  let query = req.query.q
-  console.log(`Query: ${query}`)
-  if (!query) {
-    console.warn('Empty query')
-    res.send()
-  } else {
-    query = query.trim().toLowerCase()
-    let result = await redis.get(query)
-    if (!result) {
-      console.log(`No cache for ${query}`)
-      const placeResult = await places.get(query)
-      result = JSON.stringify(placeResult.json.results)
-      await redis.setExpr30(query, result)
+  try {
+    let query = req.query.q
+    console.log(`Query: ${query}`)
+    if (!query) {
+      console.warn('Empty query')
+      res.send()
+    } else {
+      query = query.trim().toLowerCase()
+      let result = await redis.get(query)
+      if (!result) {
+        console.log(`No cache for ${query}`)
+        const placeResult = await places.get(query)
+        result = JSON.stringify(placeResult.json.results)
+
+        await redis.setExpr30(query, result)
+      }
+      console.log(`result: ${JSON.stringify(JSON.parse(result), undefined, 2)}`)
+      res.status(200).json(JSON.parse(result))
     }
-    console.log(`result: ${result}`)
-    res.status(200).json(JSON.parse(result))
+  } catch (error) {
+    console.warn(error)
+    res.status(200).send(error)
   }
 }
